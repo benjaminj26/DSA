a0f036d2b2c5779a7621d5c24ef9fb63640dbe8e
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-01-07 23:01:45 +0530

    Update

--- Infix_Postfix_Stack.c
+++ Infix_Postfix_Stack.c
@@ -11,37 +11,108 @@ int priority(char ch)
 		return 1;
 	else if(ch == '*' || ch == '/')
 		return 2;
+	else 
+		return -1; 
 }
 
-void push(char ch, char **stack)
+int push(char ch, char *stack, int top)
 {
-	
+	if(top == -1)
+	{
+		top = 0;
+		stack[top] = ch;
+		return top;
+	}
+	else
+	{
+		stack[++top] = ch;
+		return top;
+	}
 }
 
-void pop(char ch, char **stack)
+int pop(char *stack, int top)
 {
-	
+	if(top == 0)
+	{
+		printf("%c ", stack[top]);
+		top = -1;
+		return top;
+	}
+	else 
+	{
+		printf("%c ",stack[top]);
+		top--;
+		return top;
+	}
 }
 
-void main()
+int main()
 {
 	size_t buffer = 10;
+	int size = 0;
 	char *expression = (char*)calloc(buffer,1);
 	printf("Enter the expression:\n");
 	getline(&expression, &buffer, stdin);
-	char *stack = (char*)malloc(1);
+	expression[strlen(expression)-1] = '\0';
+	for(int i=0; i<strlen(expression); ++i)
+	{
+		if(expression[i] == '*')
+			size++;
+		else if(expression[i] == '/')
+			size++; 
+		else if(expression[i] == '+')
+			size++;
+		else if(expression[i] == '-')
+			size++;
+		else if(expression[i] == '^')
+			size++;
+	}
+	char stack[size];
+	int top = -1;
 	for(int i=0; i<strlen(expression); ++i)
 	{
 		char ch = expression[i];
 		int prty = priority(ch);
-		if(prty > 0)
+		if(prty >= 0)
 		{
-			push(ch,&stack);
+			if(prty > priority(stack[top]) || stack[top] == '\0')
+				top = push(ch,stack,top);
+			else if(prty < priority(stack[top]))
+			{
+				if(ch == ')')
+				{
+					while(stack[top] != '(')
+					{
+						top = pop(stack,top);
+					}
+				}
+				else
+				{
+					top = pop(stack,top);
+					top = push(ch,stack,top);
+				}
+			}
+			// else if(ch == ')')
+			// {
+			// 	while(stack[top] != '(')
+			// 	{
+			// 		top = pop(stack,top);
+			// 	}
+			// }
 		}
 		else if(isalnum(ch))
 		{
 			printf("%c",ch);
 		}
+		else if(i == size-1)
+		{
+			while(top >= 0)
+			{
+				top = pop(stack,top);
+			}
+		}
 	}
+	printf("\n");
+	free(expression);
 	return 0;
 }
