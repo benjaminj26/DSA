c252270452dd4653fcbe3d520c4cfd3d0197a0af
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-04-29 12:43:20 +0530

    Improved Heap_Sort.c

--- src/Heap_Sort.c
+++ src/Heap_Sort.c
@@ -8,37 +8,41 @@ void swap(int *heap, int index1, int index2)
     heap[index2] = temp;
 }
 
-void heapify(int *heap, int size)
+void heapify(int *heap, int size, int index)
 {
-    if(size <= 1)
-        return;
-    for(int i=size-1; i >= (size/2); i--)
-    {
-        if(i%2 == 0)
-        {
-            if(heap[i] > heap[(i/2)-1])
-            {
-                swap(heap, i, ((i/2)-1)); 
-            }
-        }
-        else
-        {
-            if(heap[i] > heap[(i/2)])
-            {
-                swap(heap, i, (i/2));
-            }
-        }
-    }
-    heapify(heap, (size)/2);
+    int largest = index;
+	int left = index * 2 + 1;
+	int right = index * 2 + 2;
+
+	if(left < size && heap[left] > heap[largest])
+	{
+		largest = left;
+	}
+
+	if(right < size && heap[right] > heap[largest])
+	{
+		largest = right;
+	}
+
+	if(index != largest)
+	{
+		swap(heap, largest, index);
+		heapify(heap, size, largest);
+	}
 }
 
 void heap_sort(int *heap, int size)
 {
-    if(size <= 1)
-        return;
-    heapify(heap, size);
-    swap(heap,0,size-1);
-    heap_sort(heap, size-1);
+   for(int i=(size/2)-1; i >= 0; --i)
+   {
+	   heapify(heap, size, i);
+   }
+
+   for(int i=size-1; i >= 0; --i)
+   {
+	   swap(heap, 0, i);
+	   heapify(heap, i, 0);
+   }
 }
 
 int main()
